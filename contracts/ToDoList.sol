6b1ca81047da6655358939a3190532fd2067f171
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2021-12-05 23:11:02 +0530

    begin

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.16;
+// pragma solidity ^0.8.10;
 
 contract ToDoList{
     uint public taskCount=0;
+
+struct Task{
+    uint id;
+    string content;
+    bool completed;
+}
+mapping(uint => Task ) public tasks;
+constructor() public {
+    createTask("Hello");
+    }
+
+function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount,_content,false);
+   
+}
 }
